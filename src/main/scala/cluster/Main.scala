1219feba7eec54300bd1a9b4aad840ef1c954adc
Janina <feder.tinte@gmail.com>
2024-05-05 23:08:25 +0200

    Final

--- src/main/scala/cluster/Main.scala
+++ src/main/scala/cluster/Main.scala
@@ -8,15 +8,10 @@ import scala.io.StdIn
 object Main {
 
   def main(args: Array[String]): Unit = {
-
-    /*val port = args(0).toInt
-    val role = args(1)*/
-
-    val port = System.getenv("port").toInt
     val role: String = System.getenv("role")
 
     // get hostname (docker)
-    val configuration = createConfiguration(port, java.net.InetAddress.getLocalHost.getHostName)
+    val configuration = createConfiguration(java.net.InetAddress.getLocalHost.getHostName)
 
     ActorSystem[Nothing](Guardian(role), "ping", configuration)
 
@@ -26,7 +21,8 @@ object Main {
 
   }
 
-  private def createConfiguration(port: Int, hostname: String): Config = ConfigFactory
-    .parseString(s"akka.remote.artery.canonical.hostname = $hostname\nakka.remote.artery.canonical.port = $port")
+  private def createConfiguration(hostname: String): Config = ConfigFactory
+    .parseString(s"akka.remote.artery.canonical.hostname = $hostname")
     .withFallback(ConfigFactory.load())
+
 }
